30ab044bdb114d9f2bea73d5eb577e78feac3de1
larryatoms <larryatoms@gmail.com>
2022-11-13 08:12:57 -0700

    First

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,35 +1,48 @@
-const Header = (props) => {
-  return <h1>{props.course}</h1>;
+const Header = ({name}) => {
+  return <h1>{name}</h1>;
 };
 
-const Content = (props) => {
+const Content = ({parts}) => {
   return (
     <p>
-      {props.part} {props.ex}
+      {parts[0].name} {parts[0].exercises}<br/>
+      {parts[1].name} {parts[1].exercises}<br/>
+      {parts[2].name} {parts[2].exercises}<br/>
     </p>
   );
 };
 
-const Total = (props) => {
-  return <p>Number of exercises {props.ex1 + props.ex2 + props.ex3}</p>;
+const Total = ({parts}) => {
+  return (
+    <p>Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>
+  );
 };
 
 const App = () => {
-  const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
+  
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
 
   return (
     <div>
-      <Header course={course} />
-      <Content part={part1} ex={exercises1} />
-      <Content part={part2} ex={exercises2} />
-      <Content part={part3} ex={exercises3} />
-      <Total ex1={exercises1} ex2={exercises2} ex3={exercises3} />
+      <Header name={course.name}/>
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   );
 };
---
+++ part1/unicafe/src/App.js
@@ -0,0 +1,48 @@
+import { useState } from 'react'
+
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
+
+const App = () => {
+  
+  const [good, setGood] = useState(0) 
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+
+  const handleGood = () => {
+    setGood(good + 1)
+  }
+
+  const handleNeutral = () => {
+    setNeutral(neutral + 1)
+  }
+
+  const handleBad = () => {
+    setBad(bad + 1)
+  }
+
+  return (
+    <div>
+      <h1>give Feedback</h1>
+      <Button handleClick={handleGood} text="Good" />
+      <Button handleClick={handleNeutral} text="Neutral" />
+      <Button handleClick={handleBad} text="Bad" />
+      <h2>statistics</h2>
+      <p>
+        good {good}<br/>
+        neutral {neutral}<br/>
+        bad {bad}<br/>
+        all {good + neutral + bad}<br/>
+        total {good + (bad * -1)}<br/>
+        average {good + (bad * -1) / (good + neutral + bad)}<br/>
+        positive {(good / (good + neutral + bad)) * 100}%<br/> 
+      </p>
+    </div>
+  )
+}
+
+export default App
