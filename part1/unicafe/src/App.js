2c12ee1e6d1773b21404e8815294d46d53f49914
larryatoms <larryatoms@gmail.com>
2022-11-13 18:25:39 -0700

    Final

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,29 +1,25 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-const Button = (props) => (
-  <button onClick={props.handleClick}>
-    {props.text}
-  </button>
-)
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>{text}</button>
+);
 
 const App = () => {
-  
-  const [good, setGood] = useState(0) 
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
   const handleGood = () => {
-    setGood(good + 1)
-  }
+    setGood(good + 1);
+  };
 
   const handleNeutral = () => {
-    setNeutral(neutral + 1)
-  }
+    setNeutral(neutral + 1);
+  };
 
   const handleBad = () => {
-    setBad(bad + 1)
-  }
+    setBad(bad + 1);
+  };
 
   return (
     <div>
@@ -33,16 +29,15 @@ const App = () => {
       <Button handleClick={handleBad} text="Bad" />
       <h2>statistics</h2>
       <p>
-        good {good}<br/>
-        neutral {neutral}<br/>
-        bad {bad}<br/>
-        all {good + neutral + bad}<br/>
-        total {good + (bad * -1)}<br/>
-        average {good + (bad * -1) / (good + neutral + bad)}<br/>
-        positive {(good / (good + neutral + bad)) * 100}%<br/> 
+        good {good} <br/>
+        neutral {neutral} <br/>
+        bad {bad} <br/>
+        all {good + neutral + bad} <br/>
+        average {(good + (bad * -1)) / (good + neutral + bad)} <br />
+        positive {(good / (good + neutral + bad)) * 100}%<br />
       </p>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
